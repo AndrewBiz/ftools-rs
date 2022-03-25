78b76688f80585bce2e4bf8d91df52dbed4b79e3
Andrew (ANB) <andrew.bizyaev@gmail.com>
2022-03-25 21:02:31 +0300

    some cleaning and refactoring

--- src/bin/ftls.rs
+++ src/bin/ftls.rs
@@ -4,35 +4,34 @@ mod ftools;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[clap(version, long_about = None)]
-#[clap(about =
-"*Keep Your Media Files In Order* (c) ANB
+#[clap(version, long_about = None, verbatim_doc_comment)]
 
-ftls scans given directories and generates list of files to standard
-output. In short it acts like 'ls' command (or 'dir' in Windows) but only for
-media files supported by ftools.
-Set DIRs to be scanned as a parameters. If no DIRs are set - current dir (.)
-will be scanned. Set FILEMASKs as a parameters - and only files matching the
-masks will be processed. If no FILEMASK is set '*.*' will be used by-default.
-To avoid unnecessary mask extraction by OS - put it in ''.
+///     *Keep Your Media Files In Order* (c) ANB
+///     ftls scans given directories and generates list of files to standard
+/// output. In short it acts like 'ls' command (or 'dir' in Windows) but only for
+/// media files supported by ftools.
+/// Set DIRs to be scanned as a parameters. If no DIRs are set - current dir (.)
+/// will be scanned. Set FILEMASKs as a parameters - and only files matching the
+/// masks will be processed. If no FILEMASK is set '*.*' will be used by-default.
+/// To avoid unnecessary mask extraction by OS - put it in ''.
+///     ftls acts as a 'source' program meaning it does not require any input
+/// from STDIN, it generates list of files based on input parameters and sends it
+/// to STDOUT. The command is intended to be used with other programs
+/// connected via pipes as a 1st command in the pipe chain, e.g.:
+/// ```
+///    ftls abc '*aaa*' | ftrename -a anb
+/// ```
+/// => scans 'abc' folder and sends all found files filtered
+/// with *aaa* to ftrename command
 
-ftls acts as a 'source' program meaning it does not require any input
-from STDIN, it generates list of files based on input parameters and sends it
-to STDOUT. The command is intended to be used with other programs
-connected via pipes as a 1st command in the pipe chain, e.g.:
-
-    ftls abc '*aaa*' | ftrename -a anb
-
-=> scans 'abc' folder and sends all found files filtered
-with *aaa* to ftrename command.")]
-struct Opt {
+struct CliArgs {
     #[clap(long)]
     /// Show supported file types
     supported_types: bool,
 }
 
 fn main() {
-    let cli_args = Opt::parse();
+    let cli_args = CliArgs::parse();
     if cli_args.supported_types {
         println!(
             "ftls supports file types: {:?}",

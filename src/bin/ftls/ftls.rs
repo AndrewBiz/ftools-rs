83e6776389e39edcac590679489dc73611920729
Andrew (ANB) <andrew.bizyaev@gmail.com>
2022-03-28 21:07:23 +0300

    clippy refactoring

--- src/bin/ftls/ftls.rs
+++ src/bin/ftls/ftls.rs
@@ -2,17 +2,15 @@
 pub fn output_file(path: &std::path::Path) {
     // checking file extension - should be exist and supported
     match path.extension() {
-        None => return,
+        None => (),
         Some(ext) => {
             let ext = ext.to_ascii_lowercase();
             match ftools::SUPPORTED_FILE_TYPE
                 .iter()
                 .position(|&supported_ext| supported_ext == ext)
             {
-                None => return,
-                Some(_) => {
-                    println!("{}", path.display());
-                }
+                None => (),
+                Some(_) => println!("{}", path.display())
             }
         }
     }

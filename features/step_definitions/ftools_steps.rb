e0df15dd0537a0328fff7cce2c62b05c08c023a5
Andrew (ANB) <andrew.bizyaev@gmail.com>
2022-03-30 22:48:40 +0300

    added step definition

--- features/step_definitions/ftools_steps.rb
+++ features/step_definitions/ftools_steps.rb
@@ -46,15 +46,13 @@ end
 # #     step %{the stdout from "#{cmd}" should contain "#{item}"}
 # #   end
 # # end
-#
-# Then(/^the stdout should not contain any of:$/) do |table|
-#   # table is a Cucumber::Ast::Table
-#   outs = table.raw.flatten
-#   outs.each do |item|
-#     step %(the stdout should not contain "#{item}")
-#   end
-# end
-#
+
+Then('the {channel} should not contain any of:') do |channel, table|
+  table.raw.flatten.each do |item|
+    step %(the #{channel} should not contain #{item}")
+  end
+end
+
 # Then(/^the output should match each of:$/) do |table|
 #   outs = table.raw.flatten
 #   outs.each do |item|

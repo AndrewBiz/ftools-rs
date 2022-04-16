a6013c7e47e5e8ed7df4cdc5943032aa83ecf800
Andrew (ANB) <andrew.bizyaev@gmail.com>
2022-04-16 23:24:38 +0300

    experiment with ranges

--- src/bin/ftrename.rs
+++ src/bin/ftrename.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world, from ftrename2");
+    let r1 = 123;
+    let r2 = r1 - 10;
+    let r = r1..=r2;
+    let i = "120";
+    if r.contains(&i.parse::<i32>().unwrap()) {
+        println!(" i={} is inside range", i);
+    }
+
+    println!("r = {:?}", r);
 }

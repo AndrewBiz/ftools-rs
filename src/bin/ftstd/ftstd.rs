110991b3d22f9b131644ad0f9081259f3b887074
Andrew Biz <andrew.bizyaev@gmail.com>
2024-01-08 13:07:29 +0000

    refactor ftrename to ftstd

---
+++ src/bin/ftstd/ftstd.rs
@@ -0,0 +1,72 @@
+use crate::CliArgs;
+use anyhow::{anyhow, Result};
+use atty::Stream;
+use std::io::BufRead;
+
+// app class
+pub struct App {
+    author: String,
+    undo: bool,
+}
+
+impl App {
+    pub fn init(args: CliArgs) -> App {
+        log::debug!("Start app init");
+        let author = args.author.unwrap_or_default().to_ascii_uppercase();
+        log::debug!("Finish app init");
+        App {
+            author,
+            undo: args.undo,
+        }
+    }
+
+    pub fn run(&self) -> Result<()> {
+        log::debug!("Start run");
+        // if program is run in terminal - quit
+        if atty::is(Stream::Stdin) {
+            return Err(anyhow!(
+                "Command should be executed in a pipe mode (e.g. ftls | ftstd -a anb)"
+            ));
+        }
+
+        // processing stdin lines
+        let stdin = std::io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line.unwrap_or_default();
+
+            match self.process_file(&line) {
+                Ok(out_fn) => ftools::output_to_stdout(&out_fn),
+                Err(e) => eprintln!(" ! {} - {}", &line, e),
+            }
+        }
+        log::debug!("Finish run");
+        Ok(())
+    }
+
+    pub fn process_file(&self, in_fn: &String) -> Result<String> {
+        log::debug!("Started processing {}", in_fn);
+
+        let mf = ftools::media_file::init(in_fn.clone(), self.author.clone())?; // TODO: not to exit in case of error
+        log::debug!("MediaFile read: {:?}", mf);
+
+        let out_fn;
+        // TODO: To be changed depending on input parameters
+        if self.undo {
+            // renaming to the original name
+            out_fn = mf.fs_path_original.to_str().unwrap_or_default().to_string();
+        } else {
+            // renaming to standard name
+            out_fn = mf.fs_path_standard.to_str().unwrap_or_default().to_string();
+        }
+
+        // renaming the file (if needed):
+        if *in_fn == out_fn {
+            log::debug!("... keeping file_name unchanged");
+        } else {
+            log::debug!("... renaming to: {}", &out_fn);
+            std::fs::rename(in_fn, &out_fn)?;
+        }
+        log::debug!("Finished processing {}", &in_fn);
+        Ok(out_fn)
+    }
+}
---
+++ src/bin/ftstd/main.rs
@@ -0,0 +1,66 @@
+#![feature(unix_sigpipe)]
+use anyhow::{Context, Result};
+
+mod ftstd;
+
+// command options and arguments
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap(version, long_about = None, verbatim_doc_comment)]
+/// *Keep Your Media Files In Order* (c) ANB
+///      ftstd renames the original input file to the ft-standard name:
+/// YYYYmmdd-HHMMSS_AAA ORIGINAL.EXT, where:
+/// YYYYmmdd-HHMMSS - Date-Time of content creation,
+/// AAA - the author nickname,
+/// ORIGINAL.EXT - the original file name (e.g. given by digital camera).
+/// By default date-time info is taken from EXIF area (in most cases DateTimeOriginal tag).
+/// If no internal tags found - FileModifiedDate is used instead
+/// Example: input file DSC03455.JPG will be renamed to 20130108-124145_ANB DSC03455.JPG
+/// ftstd acts as a 'filter' meaning it expects the input files to be passed
+/// to STDIN and after the job is done it produces STDOUT with the list of renamed
+/// files. In other words this command is intended to be used with other programs
+/// connected via pipes, e.g.:
+///     ftls | ftstd -a anb | some_other_program_taking_stdin
+/// The program is designed to be safe to re-run on the same file several times
+/// - every re-run produces the same result (idempotent behaviour).
+/// Once the file was renamed to the ft-standard name, the date-time kept in the name
+/// is considered as a master date-time of the content creation and will not be
+/// changed by re-running ftstd unless user explicitly sets correspondig option.
+/// This program in some cases uses external utility ExifTool created by Phil Harvey
+/// (http://www.sno.phy.queensu.ca/~phil/exiftool/).
+
+pub struct CliArgs {
+    #[clap(long)]
+    /// Show debug information
+    debug: bool,
+
+    // TODO! - validation via #[arg(value_parser = valid_autor)]
+    #[clap(long, short = 'a', verbatim_doc_comment)]
+    /// Sets the author nickname. The nickname should be 3 to 6 ASCII chars long (e.g. ANB)
+    author: Option<String>,
+
+    #[clap(long, short = None)]
+    /// Rename file back to it's original name
+    undo: bool,
+}
+
+#[unix_sigpipe = "sig_dfl"] // This is for correct working in pipe mode under unix-like systems
+fn main() -> Result<()> {
+    let cli_args = CliArgs::parse();
+
+    if cli_args.debug {
+        env_logger::Builder::new()
+            .filter_level(log::LevelFilter::Debug)
+            .init();
+    } else {
+        env_logger::init();
+    }
+    log::debug!("START main");
+    log::debug!("Arguments set by the user: {:?}", &cli_args);
+
+    let app = ftstd::App::init(cli_args);
+    app.run().context("Running ftstd")?;
+
+    log::debug!("FINISH main");
+    Ok(())
+}

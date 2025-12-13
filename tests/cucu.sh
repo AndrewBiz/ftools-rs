706892b48070765d2f4a6daabda9a4114d18cc52
Andrey Bizyaev <andrew.bizyaev@gmail.com>
2025-12-13 23:44:11 +0300

    Update cucu.sh

--- tests/cucu.sh
+++ tests/cucu.sh
@@ -1,15 +1,17 @@
-# usage: 
+# usage:
 #    tests/cucu.sh
 #    tests/cucu.sh -t @ftrename
 RUST_EXE=$PWD"/target/debug"
 FEATURES=$PWD"/features"
-PATH=$RUST_EXE:$FEATURES:$PATH
-#echo $PATH
+export PATH=$RUST_EXE:$FEATURES:$PATH
+echo $RUST_EXE
+echo $FEATURES
+echo $PATH
 
 echo "****** CARGO TEST ********************************"
 cargo test
 echo "******* CUCUMBER TEST ****************************"
-# cucumber -f progress summary -msi $@
+cucumber -f progress summary -msi $@
 cucumber -f progress $@
 echo "**************************************************"
-# cucumber $@
+cucumber $@
